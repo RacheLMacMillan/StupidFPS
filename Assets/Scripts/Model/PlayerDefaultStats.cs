e722489174d1248804f4074f358a93a602528700
Mitchell <r4chel.1997@gmail.com>
2025-01-23 20:00:30 +1100

    MVVM

--- Assets/Scripts/Model/PlayerDefaultStats.cs
+++ Assets/Scripts/Model/PlayerDefaultStats.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class PlayerDefaultStats : MonoBehaviour
+{	
 	public readonly ReactiveProperty<float> GravitationValue = new();
 	public readonly ReactiveProperty<float> MoveSpeed = new();
+	public readonly ReactiveProperty<float> SprintingSpeed = new();
+	public readonly ReactiveProperty<float> CrouchingSpeed = new();
 	
 	public readonly ReactiveProperty<bool> IsGrounded = new();
 	public readonly ReactiveProperty<bool> IsSprinting = new();
@@ -11,15 +13,19 @@ public class Player : MonoBehaviour
 	
 	[SerializeField] private float _gravitationValue;
 	[SerializeField] private float _moveSpeed;
+	[SerializeField, Range(0, 1)] private float _sprintingSpeed;
+	[SerializeField, Range(0, 1)] private float _crouchingSpeed;
 	
 	[SerializeField] private bool _isGrounded;
 	[SerializeField] private bool _isSprinting;
 	[SerializeField] private bool _isCrouching;
-	
+
 	private void Awake()
 	{
 		GravitationValue.Value = _gravitationValue;
 		MoveSpeed.Value = _moveSpeed;
+		SprintingSpeed.Value = _sprintingSpeed;
+		CrouchingSpeed.Value = _crouchingSpeed;
 		
 		IsGrounded.Value = _isGrounded;
 		IsSprinting.Value = _isSprinting;
