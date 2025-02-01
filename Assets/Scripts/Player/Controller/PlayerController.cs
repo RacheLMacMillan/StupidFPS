7550e71c530ee70491a886afe691adbee5464bf4
Mitchell <r4chel.1997@gmail.com>
2025-02-01 16:20:18 +1100

    Refactored

--- Assets/Scripts/Player/Controller/PlayerController.cs
+++ Assets/Scripts/Player/Controller/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
 	private PlayerCrouching _playerCrouching;
 	private PlayerSprinter _playerSprinter;
 
-    private void OnEnable()
+	private void OnEnable()
 	{
 		_playerGroundedChecker.IsGrounded.OnChanged += OnIsGroundedChanged;
 		_playerCrouching.IsCrouching.OnChanged += OnCrouchingChanged;
