2cbcf777480b70d790b8a71d128bbc68b5b861b6
Mitchell <r4chel.1997@gmail.com>
2025-02-15 19:20:19 +1100

    Comment the simple state machine

---
+++ Assets/Scripts/StateMachineSimple/PlayerStateIdle.cs
@@ -0,0 +1,26 @@
+// using UnityEngine;
+
+// public class PlayerStateIdle : FiniteStateMachineState
+// {
+// 	public PlayerStateIdle(FiniteStateMachine fsm) : base(fsm) {  }
+
+// 	public override void Enter()
+// 	{
+// 		Debug.Log("Idle is entered");
+// 	}
+	
+// 	public override void Exit()
+// 	{
+// 		Debug.Log("Idle is exited");
+// 	}
+	
+// 	public override void Update()
+// 	{
+// 		Debug.Log("Idle is updated");
+		
+// 		if (1 == 1)
+// 		{
+// 			FSM.SetState<PlayerStateWalk>();
+// 		}
+// 	}
+// }
