ee7fc52f2401e14559f6601c345ce2644aff9518
Mitchell <r4chel.1997@gmail.com>
2025-02-20 05:39:28 +1100

    Made player states

---
+++ Assets/Scripts/StateMachineAdvanced/States/PlayerIdleState.cs
@@ -0,0 +1,27 @@
+public class PlayerIdleState : PlayerState
+{
+	public PlayerIdleState(PlayerStateContext context, EPlayerState EState) : base(context, EState)
+	{
+		PlayerStateContext Context = context;
+	}
+
+    public override void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExitState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override EPlayerState GetNextState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
