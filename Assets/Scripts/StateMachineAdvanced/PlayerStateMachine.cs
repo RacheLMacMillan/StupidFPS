77317a0e9c93c735a4379c7db02bc9f6e31b93f5
Mitchell <r4chel.1997@gmail.com>
2025-02-20 05:45:31 +1100

    Add Initializing of states

--- Assets/Scripts/StateMachineAdvanced/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachineAdvanced/PlayerStateMachine.cs
@@ -11,5 +11,17 @@ public class PlayerStateMachine : BaseStateMachine<EPlayerState>
 		_player = GetComponent<Player>();
 		
 		_context = new PlayerStateContext(_player);
+		
+		InitializeStates();
+	}
+	
+	private void InitializeStates()
+	{
+		States.Add(EPlayerState.Idle, new PlayerIdleState(_context, EPlayerState.Idle));
+		States.Add(EPlayerState.Walking, new PlayerWalkingState(_context, EPlayerState.Walking));
+		States.Add(EPlayerState.Crouching, new PlayerCrouchingState(_context, EPlayerState.Crouching));
+		States.Add(EPlayerState.Sprinting, new PlayerSprintingState(_context, EPlayerState.Sprinting));
+		
+		CurrentState = States[EPlayerState.Idle];
 	}
 }
