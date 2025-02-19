b5d19813dae0595d4b5927a89c1d80bd2555087c
Mitchell <r4chel.1997@gmail.com>
2025-02-20 06:37:47 +1100

    Test states

--- Assets/Scripts/StateMachineAdvanced/States/PlayerIdleState.cs
+++ Assets/Scripts/StateMachineAdvanced/States/PlayerIdleState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class PlayerIdleState : PlayerState
 {
 	public PlayerIdleState(PlayerStateContext context, EPlayerState EState) : base(context, EState)
@@ -5,23 +7,23 @@ public class PlayerIdleState : PlayerState
 		PlayerStateContext Context = context;
 	}
 
-    public override void EnterState()
-    {
-        throw new System.NotImplementedException();
-    }
+	public override void EnterState()
+	{
+		Debug.Log("Entre Idle State");
+	}
 
-    public override void ExitState()
-    {
-        throw new System.NotImplementedException();
-    }
+	public override void ExitState()
+	{
+		Debug.Log("Exit Idle State");
+	}
 
-    public override EPlayerState GetNextState()
-    {
-        throw new System.NotImplementedException();
-    }
+	public override void UpdateState()
+	{
+		Debug.Log("Update Idle State");
+	}
 
-    public override void UpdateState()
-    {
-        throw new System.NotImplementedException();
-    }
+	public override EPlayerState GetNextState()
+	{
+		return StateKey;
+	}
 }
